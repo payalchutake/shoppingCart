bc1313cfc147ab4623eafac6432b11399844660b
payalchutake <payalchutake@gmail.com>
2020-02-05 15:04:22 +0530

    saved changes

--- shopping.js
+++ shopping.js
@@ -34,9 +34,7 @@ $('.add-to-cart').click(function (){
     itemCount ++;
 
     $('#itemCount').text(itemCount).css('display', 'block');
-    // $(this).siblings().clone().appendTo('#cartItems').append('<button class="removeItem">Remove Item</button>');
     $(this).siblings().clone().appendTo('#cartItems');
-    $('#cartItems').append('<button class="removeItem">Remove Item</button>');
     
     // Calculate Total Price
     var price = parseInt($(this).siblings().find('.price').text()); 
@@ -56,22 +54,6 @@ $('#emptyCart').click(function() {
     priceTotal = 0;
 
     $('#itemCount').css('display', 'none');
-    // $('#cartItems').text('');
+    $('#cartItems').text('');
     $('#cartTotal').text("Total: $" + priceTotal);
 }); 
-
-// Remove Item From Cart
-$('#shoppingCart').on('click', '.removeItem', function(){
-    $(this).parent().remove();  
-    itemCount --;
-    $('#itemCount').text(itemCount);
-
-    // Remove Cost of Deleted Item from Total Price
-    var price = parseInt($(this).siblings().find('.price').text());
-    priceTotal -= price;
-    $('#cartTotal').text("Total: $" + priceTotal);
-
-    if (itemCount == 0) {
-        $('#itemCount').css('display', 'none');
-    }
-});
